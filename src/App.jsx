a2ad6a62312e671e56d65312f31cc6e0eaf083be
Jonathan <jonathanvel84@gmail.com>
2023-06-28 13:59:50 -0500

    Form register Candidate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import AsideControl from "./Components/aside/AsideControl";
 import Header from "./Components/header/Header";
 import Main from "./Components/main/Main";
+import Test from "./Components/test/test";
 
 function App() {
   return (
-    <>
+    <>{/*
       <Header />
       <AsideControl />
-      <Main />
+  <Main />*/}
+      <Test />
     </>
   );
 }
